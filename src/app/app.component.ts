91fd89253b14a7850d38626c311fa7fefd947feb
caobianco1996 <caiocaobianco1@hotmail.com>
2022-08-23 17:07:34 -0300

    Sincronizando html com o formulario, submetendo o formulario, adicionado validaçao, e gerado acesso para controle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  genders = ['male', 'female'];
-  signupForm : FormGroup;
+  genders = ["male", "female"];
+  signupForm: FormGroup;
 
-  ngOnInit(){
+  ngOnInit() {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null),
-      'email': new FormControl(null),
-      'gender': new FormControl('male')
+      username: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      gender: new FormControl("male"),
     });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
   }
 }
